0d6eb7d697884e6273928dc7b83674e8b581feff
TooHangry <roberteconnolly22@gmail.com>
2021-10-20 13:50:03 -0400

    More comments

--- app/routes/list/controller.py
+++ app/routes/list/controller.py
@@ -9,7 +9,16 @@ def get_default():
     }
 
 # /lists/mine/id
+#   Gets user lists (pass userID as query param)
+
 # /lists/home/id
+#   Gets lists in a home (pass in homeIDas query param)
+
 # /lists/add
+#   Create a new list (pass in formdata with name, homeID, adminID, etc.)
+
 # /lists/remove/id
+#   Delete home with id from query param
+
 # /lists/update/id
+#   Update the list at 'id' with passed-in form data
